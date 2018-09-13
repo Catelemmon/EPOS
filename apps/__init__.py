b988dd16ef87c484fcdaca6445334e5adc404de1
Catelemmon <catelemmon@cicada.lan>
2018-09-13 10:15:11 +0800

    init project

---
+++ apps/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author  : liuheng
+# @Time    : 2018/9/13 00:50
+
+
+__author__ = "liuheng"
---
+++ apps/checkout_center/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CheckoutCenterConfig(AppConfig):
+    name = 'apps.checkout_center'
